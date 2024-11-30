c133af057a5011edf149a541d1ce56010dd7bad9
kliment <jakub.kliment@epfl.ch>
2024-11-30 11:45:33 +0100

    Types created for the poker game:
    * /shared/../types.scala: - created all possible types for the game including the game state,
    			    values for cards, phases, events and actions of the game.

---
+++ apps/shared/src/main/scala/apps/types.scala
@@ -0,0 +1,75 @@
+package apps
+
+import cs214.webapp.UserId
+import Value.*
+import apps.Suit.*
+
+type Suit = String
+
+type Value = Int
+
+type Bet = Int
+
+type Balance = Int
+
+type Hand = (Card, Card)
+
+object Suit:
+    val Heart = "♥️"
+    val Club = "♣️"
+    val Spade = "♠️"
+    val Diamond = "♦"
+    val AllSuits = Set(Heart, Club, Spade, Diamond)
+
+object Value:
+    val AllValues: Set[Value] = (2 to 14).toSet
+
+case class Card(value: Value, suit: Suit):
+    require(value >= 2 && value <= 14)
+
+    def allCards(): List[Card] =
+        val deck = 
+            for
+                value <- AllValues
+                suit <- AllSuits
+            yield Card(value, suit)
+        deck.toList
+    
+    def completeName(): String =
+        if value <= 10 then f"$value$suit"
+        else if value == 11 then f"J$suit"
+        else if value == 12 then f"Q$suit"
+        else if value == 13 then f"K$suit"
+        else f"A$suit"
+
+case class GameState(
+    playersBets: Map[UserId, Balance],
+    poolValue: Int,
+    roundBets: Map[UserId, Bet],
+    currentPlayer: UserId,
+    dealerCards: Set[Card],
+    playerCards: Map[UserId, Hand],
+    pahse: Phase
+)
+
+enum Phase:
+    case Setup, Playing, EndRound, EndGame
+
+case class View(
+    phaseView: PhaseView
+)
+
+enum PhaseView:
+    case Start
+    case Playing
+    case Done
+
+enum Event:
+    case PlayerAction(action: Action)
+    case EndGameChoice(choice: Boolean)
+
+enum Action:
+    case Check
+    case Call 
+    case Fold
+    case Raise(value: Bet)
