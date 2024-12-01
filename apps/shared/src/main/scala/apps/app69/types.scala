66bd69f5025f11c1529b8090aaf0d79384e916a7
Alexis <alexis.grillet-aubert@epfl.ch>
2024-12-01 19:38:31 +0100

    Implementation of a basic logic, still not completed and need to be tested ./types.scala : New attribut for the state ./Logic.scala : Logic implementation for the in game phase

---
+++ apps/shared/src/main/scala/apps/app69/types.scala
@@ -0,0 +1,99 @@
+package apps
+
+import cs214.webapp.UserId
+import Value.*
+import apps.*
+import scala.collection.immutable.{SortedMap}
+
+type Suit = String
+
+type Value = Int
+
+type Bet = Int
+
+type Balance = Int
+
+object Suit:
+    val Heart = "♥️"
+    val Club = "♣️"
+    val Spade = "♠️"
+    val Diamond = "♦"
+    val AllSuits = Set(Heart, Club, Spade, Diamond)
+
+object Value:
+    val AllValues: Set[Value] = (2 to 14).toSet
+
+case class Card(value: Value, suit: Suit):
+    require(value >= 2 && value <= 14)
+    
+    def completeName(): String =
+        if value <= 10 then s"$value$suit"
+        else if value == 11 then s"J$suit"
+        else if value == 12 then s"Q$suit"
+        else if value == 13 then s"K$suit"
+        else s"A$suit"
+
+    def sameSuit(other: Card): Boolean =
+        other.suit == this.suit
+    
+    def sameValue(other: Card): Boolean = 
+        other.value == this.value
+
+object AllCards:
+    def apply: Set[Card] =
+        for
+            value <- AllValues
+            suit <- Suit.AllSuits
+        yield Card(value, suit)
+
+case class Hand(first: Card, second: Card)
+
+case class GameState(
+    playerBalance: SortedMap[UserId, Balance],
+    poolValue: Int,
+    currentPlayer: UserId,
+    dealerCards: Set[Card],
+    playerCards: Map[UserId, Hand],
+    phase: Phase,
+    activePlayer: Map[UserId, Boolean],
+    smallBlind: UserId,
+    highestBetter: UserId,
+    turnBets: Map[UserId, Bet]
+)
+
+enum Phase:
+    case InGame(turn: Int)
+    case Reveal
+    case EndGame 
+
+case class View(
+    phaseView: PhaseView,
+    scoresView: ScoresView,
+    cardView: CardView
+)
+
+enum PhaseView:
+    case ChoiceSelection(currentPlayer: UserId)
+    case ChoiceMade(currentPlayer: UserId, choice: Choice)
+    case Winner(winnerId: UserId, balance: Balance)
+
+case class CardView(
+    playerCards: Hand,
+    dealerCards: Set[Card]
+)
+
+case class ScoresView(
+    playerScores: Map[UserId, Balance],
+    poolBalance: Balance
+)
+    
+enum Event:
+    case PlayerAction(choice: Choice)
+    case EndGameChoice(choice: Boolean)
+    case Ready
+
+enum Choice:
+    case Check
+    case Call 
+    case Fold
+    case Raise(value: Bet)
