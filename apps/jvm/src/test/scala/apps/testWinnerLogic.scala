9b2e624c96fd7036d9db4bb81fd2b4b95423f88e
Alexis <alexis.grillet-aubert@epfl.ch>
2024-12-02 18:37:50 +0100

    Moification of the test so that it use the new type ./testWinnerLogic.scala : Modification of the hand type following the new implementation

--- apps/jvm/src/test/scala/apps/testWinnerLogic.scala
+++ apps/jvm/src/test/scala/apps/testWinnerLogic.scala
@@ -2,7 +2,7 @@ package apps
 
 class Tests extends munit.FunSuite:
     val dealer = Set(Card(7, "♥️"), Card(6, "♥️"), Card(11, "♠️"), Card(5, "♥️"), Card(11, "♦️"))
-    val hand1 = (Card(8, "♥️"), Card(4, "♥️"))
+    val hand1 = Hand(Card(8, "♥️"), Card(4, "♥️"))
     val hand = (Card(2, "♣️"), Card(4, "♣️"))
     val game = Map(("1", hand1))
     test("Player Hand test"):
