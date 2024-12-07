1871b7cfddd648a4380f7ea0e751f5989d1988cc
GuillaumeLepin <guillaume.lepin@epfl.ch>
2024-12-07 17:58:36 +0100

    Creation of tests on the logic
    * .../TestLogic.scala: Creation of tests based on a simulation of the game
    * .../testWinnerLogic: Correction of errores and missing arguments brought
    		       by a previous commit modifying the arguments of a function

--- apps/jvm/src/test/scala/apps/testWinnerLogic.scala
+++ apps/jvm/src/test/scala/apps/testWinnerLogic.scala
@@ -1,14 +1,16 @@
 package apps
+package app69
 
 class Tests extends munit.FunSuite:
     val dealer = Set(Card(7, "♥️"), Card(6, "♥️"), Card(11, "♠️"), Card(5, "♥️"), Card(11, "♦️"))
     val hand1 = Hand(Card(8, "♥️"), Card(4, "♥️"))
     val hand = (Card(2, "♣️"), Card(4, "♣️"))
     val game = Map(("1", hand1))
+    val players = Map(("p1" -> true),("p2" -> true) )
     test("Player Hand test"):
         val win = HandValue.StraightFlush(8)
         assertEquals(win, WinnerLogic.handValue(hand1, dealer))
 
     test("Round winner"):
         val winners = Set("1")
-        assertEquals(winners, WinnerLogic.winner(game, dealer))
+        assertEquals(winners, WinnerLogic.winner(game, dealer, players))
